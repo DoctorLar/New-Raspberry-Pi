d1594e8ba968e621a495af2643e5b968240bc855
DoctorLar <doctorlar@gmail.com>
2016-05-07 23:49:31 -0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -8,7 +8,11 @@ rpi-update --assume-yes
 apt-get install apt-transport-https -y --force-yes
 
 ## install chrome
-apt-get install chromium --yes
+wget https://dl.dropboxusercontent.com/u/87113035/chromium-browser-l10n_45.0.2454.85-0ubuntu0.15.04.1.1181_all.deb
+wget https://dl.dropboxusercontent.com/u/87113035/chromium-browser_45.0.2454.85-0ubuntu0.15.04.1.1181_armhf.deb
+wget https://dl.dropboxusercontent.com/u/87113035/chromium-codecs-ffmpeg-extra_45.0.2454.85-0ubuntu0.15.04.1.1181_armhf.deb
+sudo dpkg -i chromium-codecs-ffmpeg-extra_45.0.2454.85-0ubuntu0.15.04.1.1181_armhf.deb
+sudo dpkg -i chromium-browser-l10n_45.0.2454.85-0ubuntu0.15.04.1.1181_all.deb chromium-browser_45.0.2454.85-0ubuntu0.15.04.1.1181_armhf.deb
 
 ## Add CUPS & Gutenprint
 wget https://raw.githubusercontent.com/DoctorLar/rpi-cups-gutenprint/master/install.sh
